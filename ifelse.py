cac5772f9dbcd4887fd9a162049e5c29996feb18
Bhupathiraju Deepthi <97110722+Bhupathiraju-Deepthi@users.noreply.github.com>
2022-01-25 23:22:32 +0530

    ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+snows=int(input('enter value of snows(0/1):'))
+if snows==1:
+    print('i will go out')
+else:
+    print('i will not go out')
+# froggy
+rains=int(input('enter value of rains(0/1): '))
+if rains==1:
+    print('frogs will dance')
+else:
+    print('frogs will not dance')
+    
