9cb30c9b43828af614de4c4b033b05bf1843ead9
Bhupathiraju Deepthi <97110722+Bhupathiraju-Deepthi@users.noreply.github.com>
2022-01-25 23:05:22 +0530

    listmethods.py
    this file has list methods in python

---
+++ listmethods.py
@@ -0,0 +1,27 @@
+l1=[1,2,3]
+l1.append(4)
+print(l1)
+l1.insert(2,5)
+print(l1)
+l2=[7,8,9]
+# add l2 to l1
+l1.extend(l2)
+print(l1)
+print(l1.count(2))
+print(l1.index(3))
+sorted(l1)
+print(l1)
+l1.reverse()
+print(l1)
+print(l1.pop())
+print(l1.pop(2))
+l1.remove(8)
+print(l1)
+l3=l1.copy()
+print(l3)
+l1.clear()
+print(l1)
+
+
+
+
