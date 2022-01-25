4f4c16c24999ee1fe88812fd7417759466e56104
Bhupathiraju Deepthi <97110722+Bhupathiraju-Deepthi@users.noreply.github.com>
2022-01-25 23:21:16 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,80 @@
+# assignment
+a = 3
+b = 5
+c = a + b
+print(c)
+
+# add and assign
+a = 3
+b = 5 
+# a = a + b
+a += b
+print(a)
+
+# subtract and assign
+a = 3
+b = 5  
+# a = a - b
+a -= b
+print(a)
+# multiply and assign
+a = 3
+b = 5 
+# a = a * b
+a *= b
+print(a)
+# divide and assign
+a = 3
+b = 5  
+# a = a / b
+a /= b
+print(a)
+# modulus and assign
+a = 3
+b = 5  
+# a = a % b
+a %= b
+print(a)
+# divide(floor) and assign
+a = 3
+b = 5 
+# a = a // b
+a //= b
+print(a)
+# exponent and assign
+a = 3
+b = 5
+# a = a ** b
+a **= b
+print(a)
+# bitwise and assign
+a = 3
+b = 5  
+# a = a & b
+a &= b
+print(a)
+# bitwise or and assign
+
+a = 3
+b = 5 
+# a = a | b
+a |= b
+print(a)
+# bitwise xor and assign
+a = 3
+b = 5
+# a = a ^ b
+a ^= b
+print(a)
+# bitwise right shift and assign
+a = 3
+b = 5 
+# a = a >> b
+a >>= b
+print(a)
+#bitwise left shift and assign
+a = 3
+b = 5  
+# a = a << b
+a <<= b
+print(a)
---
+++ comparison.py
@@ -0,0 +1,79 @@
+x=input('enter x value')
+y=input('enter y value')
+# comparison operators
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+print('******************************************************')
+
+# 1 set
+x=5
+y=7
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+print('******************************************************')
+# 2 set
+x=10
+y=3
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+print('******************************************************')
+
+# 3 set
+x=5
+y=5
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+print('******************************************************')
+
+# 4 set
+x=6
+y=0
+print('x > y is',x>y)
+
+print('x < y is',x<y)
+
+print('x == y is',x==y)
+
+print('x != y is',x!=y)
+
+print('x >= y is',x>=y)
+
+print('x <= y is',x<=y)
+
+
